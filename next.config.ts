e1952a8cc6af9a429bb90520228797bb67b60a4b
LiiChar <yura22875@yandex.ru>
2025-10-22 13:52:00 +0500

    fix build v.7

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
 	images: {
@@ -11,7 +12,15 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
-	
+	webpack: (config) => {
+		config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+		return config;
+	},
+	turbopack: {
+		resolveAlias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 };
 
 export default nextConfig;
